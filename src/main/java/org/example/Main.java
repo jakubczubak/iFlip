b225d823b059574ec06f14bc5bf9841daaa3f200
Kuba <czubakjakub94@gmail.com>
2025-06-14 19:08:18 +0200

    edited Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -28,7 +28,7 @@ public class Main {
 
     private static final Map<String, List<Integer>> MODEL_GROUPS = new LinkedHashMap<>() {{
         put("iPhone X–XR", Arrays.asList(0, 1, 2, 3));
-        put("iPhone 11–13", Arrays.asList(4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15));
+        put("iPhone 11–13", Arrays.asList(4, 5, 6, 7, 8, 9, 10, 11, 12, 13,14, 15));
         put("iPhone 14–16", Arrays.asList(16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27));
     }};
 
